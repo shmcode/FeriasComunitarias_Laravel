8e7440277ab62645458386cd17eb36973db44fe7
evanessscence <acbellorin@uamv.edu.ni>
2025-05-09 10:13:19 -0600

    Configuración de controller y rutas de Feria

--- feriascomunitarias/routes/web.php
+++ feriascomunitarias/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FeriaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('ferias', FeriaController::class);
